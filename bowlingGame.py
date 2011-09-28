2ddc2d9da045de24adb3c27074eb44882683a101
simoraman <simoraman@gmail.com>
2011-09-28 06:41:37 +0300

    three tests

--- bowlingGame.py
+++ bowlingGame.py
@@ -1,9 +1,26 @@
 
 class Game:
     def __init__(self):
-        self.score=0
-    def roll(self,  pins):
-        self.score+=pins
+        self.rolls=[0]*21
+        self.current_roll=0
         
-    #def score(self):
-     #   return self.score
+    def roll(self,  pins):
+        self.rolls[self.current_roll]=pins
+        self.current_roll+=1
+    
+    def score(self):
+        _score=0
+        frame_index=0
+        frame=0
+        
+        while frame<10:
+            if self.is_spare(frame_index):
+                _score+=10+self.rolls[frame_index+2]
+            else:
+                _score+=self.rolls[frame_index]+self.rolls[frame_index+1]
+            frame_index+=2
+            frame+=1
+        return _score
+        
+    def is_spare(self,  frame_index):
+        return self.rolls[frame_index]+self.rolls[frame_index+1] == 10
--- bowlingTests.py
+++ bowlingTests.py
@@ -2,20 +2,32 @@ import unittest
 import itertools
 from bowlingGame import *
 
+
 class BowlingTests(unittest.TestCase):
+    def roll_many(self,  n,  pins):
+        for _ in itertools.repeat(None,  n):
+            self.game.roll(pins)
+        
+    def roll_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        
     def setUp(self):
         self.game=Game()
     
     def test_gutter_game(self):
-        for _ in itertools.repeat(None,  20):
-            self.game.roll(0)
-        self.assertEqual(0, self.game.score)
+        self.roll_many(20, 0)
+        self.assertEqual(0, self.game.score())
         
     def test_all_ones(self):
-        for _ in itertools.repeat(None,  20):
-            self.game.roll(1)
-        self.assertEqual(20, self.game.score)
+        self.roll_many(20, 1)
+        self.assertEqual(20, self.game.score())
         
+    def test_one_spare(self):
+        self.roll_spare()
+        self.game.roll(3)
+        self.roll_many(17, 0)
+        self.assertEqual(16, self.game.score())
         
 if __name__ == '__main__':
     unittest.main()
